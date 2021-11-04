5fcaeced36ad10c75f3067956b676f1bf6747322
Jim <iggalot.iggy@gmail.com>
2021-11-04 16:32:02 -0500

    Minor refactoring.

--- MathLibraryDriver/MainWindow.xaml.cs
+++ MathLibraryDriver/MainWindow.xaml.cs
@@ -25,81 +25,12 @@ namespace MathLibraryDriver
         {
             InitializeComponent();
 
+
             TriangleList = new List<TriangleObject>();
             LineList = new List<LineObject>();
-
-            // Triangle 1
-            Vec3D vec1 = new Vec3D(-100.0f,  -100.0f,   100.0f);
-            Vec3D vec2 = new Vec3D( 100.0f,  -100.0f,   100.0f);
-            Vec3D vec3 = new Vec3D( 100.0f,   100.0f,   100.0f);
-            Vec3D vec4 = new Vec3D(-100.0f,   100.0f,   100.0f);
-            Vec3D vec5 = new Vec3D(-100.0f, -100.0f, -100.0f);
-            Vec3D vec6 = new Vec3D(100.0f, -100.0f, -100.0f);
-            Vec3D vec7 = new Vec3D(100.0f, 100.0f, -100.0f);
-            Vec3D vec8 = new Vec3D(-100.0f, 100.0f, -100.0f);
-
-            Vec2D t1 = new Vec2D(-100.0f,  -100.0f,  100.0f);
-            Vec2D t2 = new Vec2D( 100.0f,  -100.0f,  100.0f);
-            Vec2D t3 = new Vec2D( 100.0f,   100.0f,  100.0f);
-            Vec2D t4 = new Vec2D(-100.0f,   100.0f,  100.0f);
-            Vec2D t5 = new Vec2D(-100.0f, -100.0f, -100.0f);
-            Vec2D t6 = new Vec2D(100.0f, -100.0f, -100.0f);
-            Vec2D t7 = new Vec2D(100.0f, 100.0f, -100.0f);
-            Vec2D t8 = new Vec2D(-100.0f, 100.0f, -100.0f);
-
-            Pixel pixel1 = new Pixel(255, 0, 0);
-            Pixel pixel2 = new Pixel(0, 255, 0);
-            Pixel pixel3 = new Pixel(0, 0, 255);
-            Pixel pixel4 = new Pixel(0, 0, 0);
-
-            // Triangle 1
-            TriangleObject tri1 = new TriangleObject();
-            tri1.p[0] = vec1;
-            tri1.p[1] = vec2;
-            tri1.p[2] = vec3;
-            tri1.t[0] = t1;
-            tri1.t[1] = t2;
-            tri1.t[2] = t3;
-            tri1.col[0] = pixel1;
-            tri1.col[1] = pixel2;
-            tri1.col[2] = pixel3;
-
-            // Triangle 2
-            TriangleObject tri2 = new TriangleObject();
-            tri2.p[0] = vec1;
-            tri2.p[1] = vec3;
-            tri2.p[2] = vec4;
-            tri2.t[0] = t1;
-            tri2.t[1] = t3;
-            tri2.t[2] = t4;
-            tri2.col[0] = pixel1;
-            tri2.col[1] = pixel3;
-            tri2.col[2] = pixel4;
-
-            TriangleList.Add(tri1);
-            TriangleList.Add(tri2);
-
-            LineObject line = new LineObject();
-            line.p[0] = new Vec3D(-100.0f, -100.0f, -100.0f);
-            line.p[1] = new Vec3D(1000.0f, 1000.0f, 1000.0f);
-
-            line.col[0] = new Pixel(0, 0, 255);
-            line.col[1] = new Pixel(255, 0, 0);
-            LineList.Add(line);
-
             Pipeline = new DrawingPipeline(MainCanvas);
 
-            Mat4x4 transform = new Mat4x4();
-            transform = MathOps.Mat_MakeIdentity();
-
-            Pipeline.SetTransform(transform);
-
-            CameraPos = new Vec3D(0.0f, 0.0f, -250.0f);
-            CameraTarget = new Vec3D(0.0f, 0.0f, 0.0f);
-            CameraUp = new Vec3D(0.0f, 1.0f, 0.0f);
-            Pipeline.SetCamera(CameraPos, CameraTarget, CameraUp);
-
-            Pipeline.SetProjection(45, ((float)MainCanvas.Width / (float)MainCanvas.Height), 0.1f, 1000, 0.0f, 0.0f, (float) MainCanvas.Width, (float) MainCanvas.Height);
+            OnUserCreate();
 
             // Perform our rendering.
             OnUserUpdate();
@@ -144,6 +75,82 @@ namespace MathLibraryDriver
             Pipeline.Render(TriangleList, LineList, RENDERFLAGS.RENDER_CULL_CCW | RENDERFLAGS.RENDER_WIRE);
 
             //DrawingHelpersLibrary.DrawingHelpers.DrawPixel(MainCanvas, 10, 200, Brushes.Black);
+        }
+
+        private void OnUserCreate()
+        {
+            // Make some test data
+            // position data
+            Vec3D vec1 = new Vec3D(-100.0f, -100.0f, 100.0f);
+            Vec3D vec2 = new Vec3D(100.0f, -100.0f, 100.0f);
+            Vec3D vec3 = new Vec3D(100.0f, 100.0f, 100.0f);
+            Vec3D vec4 = new Vec3D(-100.0f, 100.0f, 100.0f);
+            Vec3D vec5 = new Vec3D(-100.0f, -100.0f, -100.0f);
+            Vec3D vec6 = new Vec3D(100.0f, -100.0f, -100.0f);
+            Vec3D vec7 = new Vec3D(100.0f, 100.0f, -100.0f);
+            Vec3D vec8 = new Vec3D(-100.0f, 100.0f, -100.0f);
+
+            // texture coords
+            Vec2D t1 = new Vec2D(-100.0f, -100.0f, 100.0f);
+            Vec2D t2 = new Vec2D(100.0f, -100.0f, 100.0f);
+            Vec2D t3 = new Vec2D(100.0f, 100.0f, 100.0f);
+            Vec2D t4 = new Vec2D(-100.0f, 100.0f, 100.0f);
+            Vec2D t5 = new Vec2D(-100.0f, -100.0f, -100.0f);
+            Vec2D t6 = new Vec2D(100.0f, -100.0f, -100.0f);
+            Vec2D t7 = new Vec2D(100.0f, 100.0f, -100.0f);
+            Vec2D t8 = new Vec2D(-100.0f, 100.0f, -100.0f);
+
+            // colors
+            Pixel pixel1 = new Pixel(255, 0, 0);
+            Pixel pixel2 = new Pixel(0, 255, 0);
+            Pixel pixel3 = new Pixel(0, 0, 255);
+            Pixel pixel4 = new Pixel(0, 0, 0);
+
+            // Create Triangle 1
+            TriangleObject tri1 = new TriangleObject();
+            tri1.p[0] = vec1;
+            tri1.p[1] = vec2;
+            tri1.p[2] = vec3;
+            tri1.t[0] = t1;
+            tri1.t[1] = t2;
+            tri1.t[2] = t3;
+            tri1.col[0] = pixel1;
+            tri1.col[1] = pixel2;
+            tri1.col[2] = pixel3;
+
+            // Create Triangle 2
+            TriangleObject tri2 = new TriangleObject();
+            tri2.p[0] = vec1;
+            tri2.p[1] = vec3;
+            tri2.p[2] = vec4;
+            tri2.t[0] = t1;
+            tri2.t[1] = t3;
+            tri2.t[2] = t4;
+            tri2.col[0] = pixel1;
+            tri2.col[1] = pixel3;
+            tri2.col[2] = pixel4;
+
+            // Line #1
+            LineObject line = new LineObject();
+            line.p[0] = new Vec3D(-100.0f, -100.0f, -100.0f);
+            line.p[1] = new Vec3D(1000.0f, 1000.0f, 1000.0f);
+            line.col[0] = new Pixel(0, 0, 255);
+            line.col[1] = new Pixel(255, 0, 0);
+
+            // Add to our lists of triangles and lines
+            TriangleList.Add(tri1);
+            TriangleList.Add(tri2);
+            LineList.Add(line);
+
+            // Create our camera for our view
+            CameraPos = new Vec3D(0.0f, 50.0f, -250.0f);
+            CameraTarget = new Vec3D(0.0f, 0.0f, 0.0f);
+            CameraUp = new Vec3D(0.0f, 1.0f, 0.0f);
+
+            // Create our default view matrix transform.
+            Pipeline.SetTransform(MathOps.Mat_MakeIdentity());
+            Pipeline.SetCamera(CameraPos, CameraTarget, CameraUp);
+            Pipeline.SetProjection(45, ((float)MainCanvas.Width / (float)MainCanvas.Height), 0.1f, 1000, 0.0f, 0.0f, (float)MainCanvas.Width, (float)MainCanvas.Height);
         }
     }
 }
